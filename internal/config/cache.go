52a14dbcfcd0b315dddd8c7bbc8bb5a2bd4edd32
Nikolajus Karpovas <superheromatch@gmail.com>
2020-11-23 12:21:47 +0200

    Update config package

--- internal/config/cache.go
+++ internal/config/cache.go
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2019 - 2020 MWSOFT
+  Copyright (C) 2019 - 2021 MWSOFT
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
@@ -15,12 +15,12 @@ package config
 
 // Cache holds all the configuration settings for the Redis client.
 type Cache struct {
-	Address                string `env:"REDIS_ADDRESS" default:"localhost"`
-	Port                   string `env:"REDIS_PORT" default:":6379"`
-	Password               string `env:"REDIS_PASSWORD" default:"Awesome85**"`
-	DB                     int    `env:"REDIS_DB" default:"0"`
-	PoolSize               int    `env:"REDIS_POOL_SIZE" default:"25"`
-	MinimumIdleConnections int    `env:"REDIS_MINIMUM_IDLE_CONNECTIONS" default:"10"`
-	MaximumRetries         int    `env:"REDIS_MAXIMUM_RETRIES" default:"1"`
-	MessagesKeyFormat      string `env:"REDIS_MESSAGES_KEY_FORMAT" default:"messages.for.%s"`
+	Address                string `env:"REDIS_ADDRESS" yaml:"address" default:"localhost"`
+	Port                   string `env:"REDIS_PORT" yaml:"port" default:":6379"`
+	Password               string `env:"REDIS_PASSWORD" yaml:"password" default:"Awesome85**"`
+	DB                     int    `env:"REDIS_DB" yaml:"db" default:"0"`
+	PoolSize               int    `env:"REDIS_POOL_SIZE" yaml:"pool_size" default:"25"`
+	MinimumIdleConnections int    `env:"REDIS_MINIMUM_IDLE_CONNECTIONS" yaml:"minimum_idle_connections" default:"10"`
+	MaximumRetries         int    `env:"REDIS_MAXIMUM_RETRIES" yaml:"maximum_retries" default:"1"`
+	MessagesKeyFormat      string `env:"SUPERHERO_OFFLINE_MESSAGES_REDIS_MESSAGES_KEY_FORMAT" yaml:"messages_key_format" default:"messages.for.%s"`
 }
